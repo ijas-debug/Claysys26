6139c5b74cd7b303ef6026d29e61decc29b23e21
Ijas <118385369+ijas-debug@users.noreply.github.com>
2023-07-07 21:43:03 +0530

    Add files via upload

---
+++ Controllers/AdminLoginController.cs
@@ -0,0 +1,280 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using FinalProject.Repository;
+
+namespace FinalProject.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        // GET: AdminLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Index(Admin lc)
+        {
+
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            SqlCommand sqlcomm = new SqlCommand("sp_AdminLogin", sqlconn);
+
+            sqlcomm.CommandType = CommandType.StoredProcedure;
+            sqlcomm.Parameters.AddWithValue("@EmailAddress", lc.EmailAddress);
+            sqlcomm.Parameters.AddWithValue("@Password", lc.Password);
+
+            sqlconn.Open();
+            SqlDataReader sqr = sqlcomm.ExecuteReader();
+
+            if (sqr.Read())
+            {
+                FormsAuthentication.SetAuthCookie(lc.EmailAddress, true);
+                Session["emailid"] = lc.EmailAddress.ToString();
+               return RedirectToAction("AdminHome", "AdminLogin");
+               
+
+            }
+            else
+            {
+                ViewData["message"] = "Username & Password are wrong !";
+            }
+            sqlconn.Close();
+            return View();
+        }
+
+        
+
+
+
+
+
+        public ActionResult AdminWelcome()
+        {
+            string displayimg = (string)Session["emailid"];
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "select * from [dbo].[AdminReg] where EmailAddress='" + displayimg + "'";
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("EmailAddress", Session["emailid"].ToString());
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+
+            Admin user = new Admin();
+            if (sdr.Read())
+            {
+                string s = sdr["Photo"].ToString();
+                ViewData["Img"] = s;
+                TempData["Oldimg"] = s;
+
+
+                user.FirstName = sdr["FirstName"].ToString();
+                user.LastName = sdr["LastName"].ToString();
+                user.DateOfBirth = (DateTime)sdr["DateOfBirth"];
+                user.Gender = sdr["Gender"].ToString();
+                user.PhoneNumber = sdr["PhoneNumber"].ToString();
+                user.EmailAddress = sdr["EmailAddress"].ToString();
+                user.Address = sdr["Address"].ToString();
+                user.Country = sdr["Country"].ToString();
+                user.State = sdr["State"].ToString();
+                user.City = sdr["City"].ToString();
+                user.Postcode = sdr["Postcode"].ToString();
+                user.PassportNumber = sdr["PassportNumber"].ToString();
+                user.AdharNumber = sdr["AdharNumber"].ToString();
+                user.Username = sdr["Username"].ToString();
+                user.Password = sdr["Password"].ToString();
+
+            }
+            sqlconn.Close();
+            return View(user);
+        }
+        public ActionResult Adminimgchange(HttpPostedFileBase file)
+        {
+            var emailId = (string)Session["emailid"];
+
+            string imgpath = Server.MapPath((string)TempData["Oldimg"]);
+            string fileimgpath = imgpath;
+            FileInfo fi = new FileInfo(fileimgpath);
+            if (fi.Exists)
+            {
+                fi.Delete();
+            }
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string filename = Path.GetFileName(file.FileName);
+                string filepath = Path.Combine(Server.MapPath("/Admin-Images/"), filename);
+                file.SaveAs(filepath);
+
+                string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+                using (SqlConnection sqlconn = new SqlConnection(mainconn))
+                {
+                    sqlconn.Open();
+                    string sqlquery = "UPDATE [dbo].[AdminReg] SET  Photo = @Photo WHERE EmailAddress = @EmailAddress";
+                    SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+                    sqlcomm.Parameters.AddWithValue("@Photo", "/Admin-Images/" + filename);
+                    sqlcomm.Parameters.AddWithValue("@EmailAddress", emailId);
+                    sqlcomm.ExecuteNonQuery();
+
+                }
+            }
+
+            return RedirectToAction("AdminWelcome", "AdminLogin");
+        }
+
+        //to view user 
+
+        User_Repository usersDAL = new User_Repository();
+        // GET: Product
+        public ActionResult UsersView()
+        {
+            var UsersList = usersDAL.GetAllUsers();
+
+            if (UsersList.Count == 0)
+            {
+                TempData["InfoMessage"] = "Currently Users not available in the Database";
+            }
+            return View(UsersList);
+        }
+
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var user = usersDAL.GetUsersByID(id).FirstOrDefault();
+
+                if (user == null)
+                {
+                    TempData["InfoMessage"] = "Product not available with ID " + id.ToString();
+                    return RedirectToAction("UsersView");
+                }
+                return View(user);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var user = usersDAL.GetUsersByID(id).FirstOrDefault();
+
+            if (user == null)
+            {
+                TempData["InfoMessage"] = "User not available with ID " + id.ToString();
+                return RedirectToAction("Users View");
+            }
+            return View(user);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost, ActionName("Edit")]
+        public ActionResult UpdateUsers(UserClass user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bool IsUpdated = usersDAL.UpdateUsers(user);
+
+                    if (IsUpdated)
+                    {
+                        TempData["SuccessMessage"] = "User details  updated successfully...!";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to Update";
+                    }
+                }
+
+                return RedirectToAction("UsersView");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var product = usersDAL.GetUsersByID(id).FirstOrDefault();
+
+                if (product == null)
+                {
+                    TempData["InfoMessage"] = "User not available with ID " + id.ToString();
+                    return RedirectToAction("UsersView");
+                }
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id, FormCollection collection)
+        {
+
+            try
+            {
+                string result = usersDAL.DeleteProduct(id);
+
+                if (result.Contains("deleted"))
+                {
+                    TempData["SuccessMessage"] = result;
+
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = result;
+
+                }
+                return RedirectToAction("UsersView");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+
+        public ActionResult AdminHome()
+        {
+            ViewBag.Message = "Admin Home page";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Repository/User_Repository.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using FinalProject.Models;
+
+namespace FinalProject.Repository
+{
+    public class User_Repository
+    {
+
+        String conString = ConfigurationManager.ConnectionStrings["Myconnection"].ToString();
+
+        //Get All Products
+        public List<UserClass> GetAllUsers()
+        {
+            List<UserClass> UserList = new List<UserClass>();
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetAllUsers";
+                SqlDataAdapter sqlDA = new SqlDataAdapter(command);
+                DataTable dtUsers = new DataTable();
+
+                connection.Open();
+                sqlDA.Fill(dtUsers);
+                connection.Close();
+
+                foreach (DataRow dr in dtUsers.Rows)
+                {
+                    UserList.Add(new UserClass
+                    {
+                        ID = Convert.ToInt32(dr["ID"]),
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]),
+                        Gender = dr["Gender"].ToString(),
+                        PhoneNumber = dr["PhoneNumber"].ToString(),
+                        EmailAddress = dr["EmailAddress"].ToString(),
+                        Address = dr["Address"].ToString(),
+                        Country = dr["Country"].ToString(),
+                        State = dr["State"].ToString(),
+                        City = dr["City"].ToString(),
+                        Postcode = dr["Postcode"].ToString(),
+                        PassportNumber = dr["PassportNumber"].ToString(),
+                        AdharNumber = dr["AdharNumber"].ToString(),
+                        Username = dr["Username"].ToString(),
+                    });
+                }
+            }
+            return UserList;
+        }
+
+
+        //Get All Userss by ID
+        public List<UserClass> GetUsersByID(int ID)
+        {
+            List<UserClass> UserList = new List<UserClass>();
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetUsersByID";
+                command.Parameters.AddWithValue("@ID", ID);
+                SqlDataAdapter sqlDA = new SqlDataAdapter(command);
+                DataTable dtProducts = new DataTable();
+
+                connection.Open();
+                sqlDA.Fill(dtProducts);
+                connection.Close();
+
+                foreach (DataRow dr in dtProducts.Rows)
+                {
+                    UserList.Add(new UserClass
+                    {
+                        ID = Convert.ToInt32(dr["ID"]),
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]),
+                        Gender = dr["Gender"].ToString(),
+                        PhoneNumber = dr["PhoneNumber"].ToString(),
+                        EmailAddress = dr["EmailAddress"].ToString(),
+                        Address = dr["Address"].ToString(),
+                        Country = dr["Country"].ToString(),
+                        State = dr["State"].ToString(),
+                        City = dr["City"].ToString(),
+                        Postcode = dr["Postcode"].ToString(),
+                        PassportNumber = dr["PassportNumber"].ToString(),
+                        AdharNumber = dr["AdharNumber"].ToString(),
+                        Username = dr["Username"].ToString(),
+                    });
+                }
+            }
+            return UserList;
+        }
+
+
+        //Update Products
+        public bool UpdateUsers(UserClass user)
+        {
+            int i = 0;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_UpdateUsers", connection);
+
+
+                command.CommandType = CommandType.StoredProcedure;
+                
+                command.Parameters.AddWithValue("@ID", user.ID);
+                command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                command.Parameters.AddWithValue("@LastName", user.LastName);
+                command.Parameters.AddWithValue("@DateOfBirth", user.DateOfBirth);
+                command.Parameters.AddWithValue("@Gender", user.Gender);
+                command.Parameters.AddWithValue("@PhoneNumber", user.PhoneNumber);
+                command.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+                command.Parameters.AddWithValue("@Address", user.Address);
+                command.Parameters.AddWithValue("@Country", user.Country);
+                command.Parameters.AddWithValue("@State", user.State);
+                command.Parameters.AddWithValue("@City", user.City);
+                command.Parameters.AddWithValue("@Postcode", user.Postcode);
+                command.Parameters.AddWithValue("@PassportNumber", user.PassportNumber);
+                command.Parameters.AddWithValue("@AdharNumber", user.AdharNumber);
+                command.Parameters.AddWithValue("@Username", user.Username);
+
+                connection.Open();
+                i = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Delete Product
+        public string DeleteProduct(int userid)
+        {
+            string result = "";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("SP_DELETEUSER", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ID",userid);
+                command.Parameters.Add("@OUTPUTMESSAGE", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                result = command.Parameters["@OUTPUTMESSAGE"].Value.ToString();
+                connection.Close();
+            }
+            return result;
+        }
+
+    }
+}
